bac663772c660f9e8cad332356e48b662ba3aeb5
Franz <franzj.pretorius.2003@gmail.com>
2025-02-20 16:24:37 +0200

    Changed colour of close button

--- src/ReviewAnswersPopup.jsx
+++ src/ReviewAnswersPopup.jsx
@@ -128,7 +128,7 @@ const ReviewAnswersPopup = ({ answers, email, updateAnswers, fixedMessages }) =>
             )}
 
             <button 
-              className="bg-gray-500 text-white px-4 py-2 mt-4 rounded-lg w-full"
+              className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-lg w-full"
               onClick={() => {
                 setShowPopup(false);
                 setSelectedQuestion(null);
