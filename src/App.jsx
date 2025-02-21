f68ba52f04c39f8e13ba93d5759796b286ce70a1
Franz <franzj.pretorius.2003@gmail.com>
2025-02-21 08:29:49 +0200

    Made changes before big design overhaul

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
+import "./App.css"
 import ReviewAnswersPopup from "./ReviewAnswersPopup";
 
 const Chatbot = () => {
